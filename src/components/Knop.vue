b2f10064f2794b927f52f924d61380e6633594d7
Atlas <99806895+AtlasMountains@users.noreply.github.com>
2022-09-06 13:45:49 +0200

    accessibility: add focus for keyboard users

--- src/components/Knop.vue
+++ src/components/Knop.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    class="font-semibold text-center py-2 hover:bg-opacity-0 ease-in-out duration-300"
+    class="font-semibold text-center py-2 hover:bg-opacity-0 focus:bg-opacity-0 ease-in-out duration-300"
     :href="link"
     target="_blank"
   >
